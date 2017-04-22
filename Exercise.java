a49f288689b83197439510212eeeef2704473328
erikkaBaguio <erikka.baguio@gmail.com>
2017-04-22 09:05:40 +0800

    Exercise 12-revised

--- Exercise.java
+++ Exercise.java
@@ -1,11 +1,19 @@
-// Write a single line comment anywhere you want. It can be anything! (Make sure it starts with //)
+/*
+1.Write a single line comment anywhere you want. It can be anything! (Make sure it starts with //)
+2.Set the boolean variable isComplete to true.
+3.Set the int variable awesomeLevel equal to 121.
+4.Set the int variable epicLevel equal to awesomeLevel multiplied by 2.
+5.Uncomment the last line so that the console prints out the value of epicLevel.
+*/
 
 public class Exercise
 {
 	public static void main(String[] args)
 	{
-		
-		//System.out.println(3 != 4);
-	
+		boolean isComplete = true;
+		int awesomeLevel = 121;
+		int epicLevel = awesomeLevel * 2;		
+		System.out.println(epicLevel);
+			
 	}	
 }
