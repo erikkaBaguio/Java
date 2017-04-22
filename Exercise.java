cdc95c29a5b8c61bf514fd3799e0c34d9ae02bd5
erikkaBaguio <erikka.baguio@gmail.com>
2017-04-22 09:02:18 +0800

    Exercise 11

--- Exercise.java
+++ Exercise.java
@@ -1,10 +1,11 @@
-// Use one of the relational operators above to compare any two integers.
+// Use any equality operator to directly compare two Boolean values. Do not declare any variables.
+
 public class Exercise
 {
 	public static void main(String[] args)
 	{
 		
-		System.out.println(5 <= 9);
+		System.out.println(3 != 4);
 	
 	}	
 }
