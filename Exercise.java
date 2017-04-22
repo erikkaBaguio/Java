fed50ccde46e68f8b82257b35b4b8b0c10a257c8
erikkaBaguio <erikka.baguio@gmail.com>
2017-04-22 09:02:55 +0800

    Exercise 12

--- Exercise.java
+++ Exercise.java
@@ -1,11 +1,11 @@
-// Use any equality operator to directly compare two Boolean values. Do not declare any variables.
+// Write a single line comment anywhere you want. It can be anything! (Make sure it starts with //)
 
 public class Exercise
 {
 	public static void main(String[] args)
 	{
 		
-		System.out.println(3 != 4);
+		//System.out.println(3 != 4);
 	
 	}	
 }
