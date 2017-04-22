1f92161f6ec01059115d557d6840c911ea14fbee
erikkaBaguio <erikka.baguio@gmail.com>
2017-04-22 08:58:57 +0800

    Exercise 8

--- Exercise.java
+++ Exercise.java
@@ -1,16 +1,12 @@
-//The statement on line 4 prints out Noise!. Ask Java to ignore it by commenting it out. Place two forward slashes (//) at the beginning of line 4.
-
-//Write a multi-line comment that begins anywhere after the single line comment you just wrote. Again, the comment can say anything you like.
+//Set the int variable myNumber equal to the product of two numbers.
 
 public class Exercise
 {
 	public static void main(String[] args)
 	{
 		
-		//System.out.println("Noise!");
-		
-
-		/*This is just a comment*/
+		int myNumber = 5 * 5;
+		System.out.println(myNumber);
 	
 	}	
 }
