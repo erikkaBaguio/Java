3bdcd2a92740207189e1dccd2c4411f0aa790653
erikkaBaguio <erikka.baguio@gmail.com>
2017-04-22 08:50:18 +0800

    Exercise 2

--- Exercise.java
+++ Exercise.java
@@ -1,7 +1,12 @@
+// Type any whole number in between the parentheses of System.out.println();
+
+// int data types do not need to be typed within quotes, so you can avoid using quotes this time.
+
 public class Exercise
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Ma. Erikka M. Baguio");
+		
+		System.out.println(12);
 	}	
 }
